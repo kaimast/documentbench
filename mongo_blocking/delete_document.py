95495a8c3e8faed68b39d8f384b0e89bb360fb1d
Kai Mast <mail@kai-mast.de>
2014-12-14 10:36:39 -0500

    Fixed Document Removal

--- mongo_blocking/delete_document.py
+++ mongo_blocking/delete_document.py
@@ -6,7 +6,7 @@ from common.globals import EntryPrefix
 # Run
 class DeleteDocument(MongoBlockingBench):
         def do_bench_call(self, i):
-                self.db.bench.remove({'_id', EntryPrefix + str(i)},  w=1)
+                self.db.bench.remove({'_id' : EntryPrefix + str(i)},  w=1)
 
         def name(self): 
                 return "Delete Document"
