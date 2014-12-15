56bcc543338391f74234b5856dd61e4bbef8cb69
Kai Mast <mail@kai-mast.de>
2014-12-15 12:26:12 -0500

    Fixes for bulk put

--- mongo_bulk/put_document.py
+++ mongo_bulk/put_document.py
@@ -2,12 +2,14 @@
 
 from mongo_bulk.mongo_bulk_bench import MongoBulkBench
 from common.globals import NewEntryPrefix
+from common.docbuilder import create_document
 
 # Run
 class PutDocument(MongoBulkBench):
         def do_bench_call(self, i):
                 doc = create_document(NewEntryPrefix, i)
-                bulk_op.insert(doc)
+		# There are no save operations in bulk
+                self.bulk_op.find({'_id': doc['_id']}).upsert().replace_one(doc)
 
         def name(self): 
                 return "Put Document"
